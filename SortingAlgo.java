973bb74fa3aebaa2ce61f5e4908f6600eb0000cc
Anuj Rawat <rawatanuj532@gmail.com>
2026-09-04 13:00:43 +0530

    Add insertion sort practice

--- SortingAlgo.java
+++ SortingAlgo.java
@@ -33,13 +33,39 @@ public class SortingAlgo{
             arr[minIdx]  = temp;
         }
     }
-    public static void main(String args[]){
-        int arr[] = {5,4,1,2,3};
+    public static void insertionSort(int arr[]){
 
-        selectionSort(arr);
-        for(int value : arr){
-            System.out.print(value +" ");
+        int n = arr.length;
+        
+        for (int i = 1; i < n; i++) {
+            int curr = i;
+            int currValue = arr[curr];
+            int prev = i-1;
+            while ( prev>=0 && currValue < arr[prev]) {
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1] = currValue;
         }
+    }
+    public static void main(String args[]){
+
+        int arr [] = {4,1,5,2,3};
+        insertionSort(arr);
+        for (int value : arr) {
+            System.out.print(value+" ");
+        }
+
+
+
+        // int arr[] = {5,4,1,2,3};
+
+        // selectionSort(arr);
+        // for(int value : arr){
+        //     System.out.print(value +" ");
+        // }
+
+
 
     // int ans[] = bubbleSort(arr);
     // System.out.println(Arrays.toString(ans));
