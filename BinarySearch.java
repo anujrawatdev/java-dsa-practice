38a7003488b24cfa44a027f85bca3e5f26f9381e
Anuj Rawat <rawatanuj532@gmail.com>
2026-09-06 15:01:01 +0530

    feat: add lower bound, upper bound, and total occurrence functions

--- BinarySearch.java
+++ BinarySearch.java
@@ -25,10 +25,74 @@ public class BinarySearch{
         return -1;
     }
 
-public static void main(String[] args) {
-    int arr[] = {11,12,13,14,15,16,17,18,20,23,25,26,27};
+    public static int lowerBound(int arr[], int target ){
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        int ans = n ;
+        while (start<=end) {
+            int mid = start + (end - start)/2;
 
-   System.out.println(binarySearch(arr,12));
+            if(arr[mid] >=target){
+               ans = mid;
+
+               end = mid -1;
+            }
+            else{
+              start = mid + 1;
+            }
+        }
+         
+        return ans;
+
+    }
+
+    public static int upperBound(int arr[] , int target){
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        int ans = n ;
+
+        while (start<= end) {
+
+            int mid = start +(end-start)/2;
+            if(arr[mid] > target){
+             
+                ans = mid;
+             
+                end = mid-1;
+            
+            }
+            else{
+               start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public static int totalOccurence(int arr[],int target){
+        int lowerBound = lowerBound(arr, target);
+        int upperBound = upperBound(arr, target);
+        int ans = upperBound - lowerBound;
+        return ans;
+    }
+public static void main(String[] args) {
+
+    int arr[] = {10,20,30,30,30,30,30,70,70,80};
+    int target = 30;
+    int ans = totalOccurence(arr, target);
+    System.out.println("ans "+ans);
+
+
+//     int arr[] = {10,20,30,30,30,30,70,80};
+//     int target = 30;
+//     int ans = upperBound(arr,target);
+//    System.out.println("ans:" + ans);
+
+//     int arr[] = {10,20,30,30,30,40,40,50,60};
+//     int target = 30;
+//     int ans = lowerBound(arr,target);
+//    System.out.println("ans:" + ans);
 
 }
 
