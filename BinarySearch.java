5d6c73a06b17e623ae2c80b63704702b68be4c70
Anuj Rawat <rawatanuj532@gmail.com>
2026-09-13 15:30:45 +0530

    Solved Aggressive Cows with Binary Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -316,11 +316,55 @@ public class BinarySearch{
         }
         return ans;
     }
-public static void main(String[] args) {
+
+    public static boolean isValidSol(int arr[],int k ,int mid){
+        int cowCounter = 1;
+        int lastPosition = 0;
+
+       for (int i = 1; i < arr.length; i++) {
+        if(arr[i]-arr[lastPosition] >= mid){
+            cowCounter++;
+            lastPosition = i;
+
+            if(cowCounter == k){
+               return true;
+            }
+        }
+       }
+       return false;
+    }
+    public static int agressiveCows(int arr[],int k){
+
+        Arrays.sort(arr);
+        int n = arr.length;
+        int s = 0 ;
+        int e = arr[n-1] - arr[0];
+        int ans = -1;
         
-    int arr[] = {10,20,30,40};
-    int k = 2;
-    System.out.println(paintingBoard(arr, k));
+        while (s<=e) {
+            int mid = s + (e-s)/2;
+
+            if(isValidSol(arr,k,mid)){
+                ans = mid;
+                s = mid+1;
+            }
+            else{
+                e = mid - 1;
+            }
+
+        }
+        return ans;
+    }
+
+public static void main(String[] args) {
+
+    int arr[]={1,2,4,8,9};
+    int k = 3;
+    System.out.println(agressiveCows(arr, k));
+        
+    // int arr[] = {10,20,30,40};
+    // int k = 2;
+    // System.out.println(paintingBoard(arr, k));
 
 
 
